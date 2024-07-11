48c1ae76d42efa56b3dfbef65fd53312268c2769
suraj B R <suraj.b.r.3636@gmail.com>
2024-07-11 23:01:50 +0530

    files added

---
+++ Personal-portfolio both front & back-end/app.js
@@ -0,0 +1,43 @@
+// ------------------------About ---------------------------
+var tablinks = document.getElementsByClassName("tab-links")
+var tabcontents = document.getElementsByClassName("tab-contents")
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link")
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab")
+  }
+  event.currentTarget.classList.add("active-link")
+  document.getElementById(tabname).classList.add("active-tab")
+}
+// -------------------------------2nd------------------------
+
+var sidemeu = document.getElementById("sidemenu")
+
+function openmenu() {
+  sidemeu.style.right = "0"
+}
+function closemenu() {
+  sidemeu.style.right = "-200px"
+}
+
+// ---------------------Contact form sheets-------------
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwu-y7wDggHM-tWIndK4bIDUFmobmBPJO4XoQcjZrtJku5nGbCxqv42IrBLfJg2GfenpA/exec" // add your own app script link here
+const form = document.forms["submit-to-google-sheet"]
+const msg = document.getElementById("msg")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully"
+      setTimeout(function () {
+        msg.innerHTML = ""
+      }, 5000)
+      form.reset()
+    })
+    .catch((error) => console.error("Error!", error.message))
+})
